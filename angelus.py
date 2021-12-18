b0e388180d22f34b07bb095933a567c7d320dd0d
John Khoo <john_khoo@u.nus.edu>
2021-12-18 10:14:40 +0800

    Add message helpers, add scheduling functionality, and add checks against existing schedule

--- angelus.py
+++ angelus.py
@@ -9,6 +9,7 @@
 
 from ctypes.util import find_library
 from ctypes import *
+from random import randint
 import json
 import os
 import sys
@@ -18,10 +19,55 @@ import socket
 # load secrets
 decoder = json.JSONDecoder()
 with open('secrets.json', 'r') as f:
-    SECRETS = f.read().replace('\n','')
+    secret_str = f.read().replace('\n','')
+    SECRETS = decoder.decode(secret_str)
 SOCK_PATH = "./angelus.sock"
 TDJSON_PATH = "/home/aquohn/Software/td/tdlib/lib/libtdjson.so"
 BUFSIZE = 4096
+RAND_UPPER, RAND_LOWER = 0, 2**32
+TOL_SECS = 3 * 60
+
+ANGELUS = """
+The Angel of the Lord declared to Mary: And she conceived of the Holy Spirit. Hail Mary...
+
+Behold the handmaid of the Lord: Be it done unto me according to Thy word. Hail Mary...
+
+And the Word was made Flesh:
+And dwelt among us. Hail Mary...
+
+Pray for us, O Holy Mother of God, that we may be made worthy of the promises of Christ. 
+
+Let us pray:
+
+Pour forth, we beseech Thee, O Lord, Thy grace into our hearts; that we, to whom the incarnation of Christ, Thy Son, was made known by the message of an angel, may by His Passion and Cross be brought to the glory of His Resurrection, through the same Christ Our Lord. Amen.
+"""
+
+EXAMEN = """
+As the day draws to a close, let us take a few minutes to examine our day in God's presence. Begin by acknowledging the presence of God, giving thanks for the day that has passed, and asking for His light as you review it. Then, step through the events of the day, from when you woke up, praising God with gratitude for the blessings you have received and humbly asking His forgiveness for the times you have failed Him. Here are some questions to help in our reflection!
+
+- Do I love God? Do I prioritise and seek to glorify Him, to worship Him as He deserves, and to understand and follow His commandments?
+- Am I constantly aware of the presence of God in everything I do, and His love for me? Do I draw peace and joy from the knowledge that I am His child, or do I allow worldly cares to overwhelm me? Do I trust in God's love?
+- Did things go according to plan today, or even better than expected? Have I been grateful for these occasions?
+- Did anyone go out of his way today to help me? Did I acknowledge him with gratitude?
+- Did I have any unpleasant interactions with people today? Why were they unpleasant?
+- Have I fulfilled my responsibilities to my family, friends, and work? Do I love my family and friends? Do they know that I love them?
+- Have I wasted my time today, or caused other people to waste their time? Have I used my resources (talents, money, belongings, etc.) well?
+- Do I bear the difficulties of each day with patience, in union with Christ? Am I overly attached to comfort, preference, and convenience?
+- Have I entertained prideful, lustful, hateful, envious, etc. thoughts instead of resisting them?
+- Have I been dishonest? Have I discussed the faults of others unnecessarily?
+- Do I humbly acknowledge my faults and ask forgiveness from people? Do I forgive others? Do I seek to grow from my mistakes, and help others to grow too?
+- Have I taken reasonable care of my health?
+- Have I tried to bring the love of Christ to others today, and to lead them to Him?
+
+No matter how well or badly your day has gone, God loves you with an eternal love. If we open our hearts to Him, He will give us the grace to do better. Even the gravest of sins can be forgiven, and our relationship with God restored, in Confession. Let's keep praying for each other!
+"""
+
+def unlink_sock(path):
+    try:
+        os.unlink(path)
+    except OSError:
+        if os.path.exists(path):
+            raise RuntimeError(f"Something else already exists at {path}!")
 
 # load shared library
 tdjson = CDLL(TDJSON_PATH)
@@ -57,7 +103,7 @@ c_on_log_message_callback = log_message_callback_type(on_log_message_callback)
 _td_set_log_message_callback(2, c_on_log_message_callback)
 
 # setting TDLib log verbosity level to 1 (errors)
-print(str(td_execute({'@type': 'setLogVerbosityLevel', 'new_verbosity_level': 1, '@extra': 1.01234})).encode('utf-8'))
+td_execute({'@type': 'setLogVerbosityLevel', 'new_verbosity_level': 1, '@extra': 1.01234})
 
 # create client
 client_id = _td_create_client_id()
@@ -77,17 +123,48 @@ def td_receive():
 td_send({'@type': 'getAuthorizationState'})
 
 # compute unix timestamps to send messages tomorrow
-tmrw = dtime.datetime.now() + dtime.timedelta(days=1)
+sgtz = dtime.timezone(dtime.timedelta(hours=8)) # ensure timezone is correct
+tmrw = dtime.datetime.now(sgtz) + dtime.timedelta(days=1)
 def time_to_epoch(dt, h, m):
     ndt = dtime.datetime(dt.year, dt.month, dt.day,
             hour=h, minute=m)
     return ndt.timestamp()
 
 morning_angelus = int(time_to_epoch(tmrw, 6, 0))
-afternoon_angelus = int(time_to_epoch(tmrw, 12, 0))
-eveing_angelus = int(time_to_epoch(tmrw, 18, 0))
+noon_angelus = int(time_to_epoch(tmrw, 12, 0))
+evening_angelus = int(time_to_epoch(tmrw, 18, 0))
 examen = int(time_to_epoch(tmrw, 22, 30))
 
+def sched_msg(text, timestamp):
+    extra = randint(RAND_UPPER, RAND_LOWER)
+    td_send({'@type': 'sendMessage', 'chat_id': SECRETS['channel_id'], 
+        'options': {'@type': 'messageSendOptions', 'scheduling_state': 
+            {'@type': 'messageSchedulingStateSendAtDate', 'send_date': timestamp} }, 
+        'input_message_content': {'@type': 'inputMessageText',
+            'text': {'@type': 'formattedText', 'text': text}},
+        '@extra': extra})
+    
+
+def msg_before(timestamp): 
+    extra = randint(RAND_UPPER, RAND_LOWER)
+    td_send({'@type': 'getChatMessageByDate', 'chat_id': SECRETS['channel_id'], 
+        'date': timestamp, '@extra': extra})
+    return extra
+
+def get_sched():
+    extra = randint(RAND_UPPER, RAND_LOWER)
+    td_send({'@type': 'getChatScheduledMessages', 'chat_id': SECRETS['channel_id'], 
+        '@extra': extra})
+    return extra
+
+def wait_extra(extra):
+    while True:
+        event = td_receive()
+        if event and event.get('@extra', None) == extra:
+            print(event)
+            return event
+
+auth_ready = False
 # main events cycle
 while True:
     event = td_receive()
@@ -99,7 +176,6 @@ while True:
             # if client is closed, we need to destroy it and create new client
             if auth_state['@type'] == 'authorizationStateClosed':
                 td_send({'@type': 'getAuthorizationState'})
-                continue
 
             # set TDLib parameters
             # you MUST obtain your own api_id and api_hash at https://my.telegram.org
@@ -112,9 +188,10 @@ while True:
                                                        'api_id': SECRETS['api_id'],
                                                        'api_hash': SECRETS['api_hash'],
                                                        'system_language_code': 'en',
-                                                       'device_model': 'Desktop',
+                                                       'device_model': 'Olinuxino',
                                                        'application_version': '0.1',
-                                                       'enable_storage_optimizer': True}})
+                                                       'enable_storage_optimizer': True},
+                                                       })
 
             # set an encryption key for database to let know TDLib how to open the database
             if auth_state['@type'] == 'authorizationStateWaitEncryptionKey':
@@ -132,5 +209,35 @@ while True:
                 code = sock.recv(BUFSIZE).decode(encoding='utf-8')[:6]
                 td_send({'@type': 'checkAuthenticationCode', 'code': code})
 
-        # main actions here
+            if auth_state['@type'] == 'authorizationStateReady':
+                auth_ready = True
+
+            print(event)
+            continue
+
+        if not auth_ready:
+            continue
+
+        sched_msgs = wait_extra(get_sched()).get('messages', None)
+        # if not sched_msgs:
+        pending_scheds = {morning_angelus: ANGELUS, noon_angelus: ANGELUS, evening_angelus: ANGELUS,
+                examen: EXAMEN}
+        for msg in sched_msgs:
+            try:
+                sched_timestamp = msg.get('scheduling_state', None).get('send_date', None)
+                sched_text = msg.get('content', None).get('text', None).get('text', None)
+            except AttributeError:
+                continue
+            rmlist = []
+            for time in pending_scheds.keys():
+                if abs(sched_timestamp - time) < TOL_SECS:
+                    rmlist.append(time)
+            for rmtime in rmlist:
+                pending_scheds.pop(rmtime)
+
+        for time, msg in pending_scheds.items():
+            sched_msg(msg, time)
+
         break
+
+        # main events here
